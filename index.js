355260815c0acfdccc82452c2e529391c22d370c
ducksandgoats <mrtraderjoseph@gmail.com>
2022-10-15 17:27:30 -0500

    package

--- index.js
+++ index.js
@@ -26,14 +26,6 @@ module.exports = function makeGemini (opts = {}) {
     return theData
   }
 
-  async function moveToData(data){
-    let mainData = ''
-    for await (const test of data){
-      mainData = mainData + test.toString()
-    }
-    return mainData
-  }
-
   return makeFetch(async (request) => {
 
     const { url, method, headers: reqHeaders, body, signal, referrer } = request
@@ -41,9 +33,6 @@ module.exports = function makeGemini (opts = {}) {
     if(signal){
       signal.addEventListener('abort', takeCareOfIt)
     }
-
-    const mainReq = !reqHeaders.accept || !reqHeaders.accept.includes('application/json')
-    const mainRes = mainReq ? 'text/html; charset=utf-8' : 'application/json; charset=utf-8'
 
     try {
       const toRequest = new URL(url, referrer)
@@ -63,7 +52,7 @@ module.exports = function makeGemini (opts = {}) {
           } else {
             const { statusCode, statusMessage: statusText } = res
 
-            const headers = {'Content-Type': mainRes}
+            const headers = {}
 
             const data = res
 
@@ -76,8 +65,6 @@ module.exports = function makeGemini (opts = {}) {
           }
         })
       })
-      mainObj.data = await moveToData(mainObj.data)
-      mainObj.data = mainReq ? [`<html><head><title>${toRequest.toString()}</title></head><body>${mainObj.data}</body></html>`] : [mainObj.data]
       return sendTheData(signal, mainObj)
     } catch (error) {
       return sendTheData(signal, {statusCode: 500, headers: {'Content-Type': mainRes}, data: mainReq ? [`<html><head><title>Gemini-Handle</title></head><body>${JSON.stringify(error.stack)}</body></html>`] : [JSON.stringify(error.stack)]})
