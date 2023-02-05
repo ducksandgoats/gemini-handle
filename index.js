2d4d576787917548e8cf3b15fda89e7dc475d63e
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-05 17:15:08 -0600

    update package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = async function makeGemini(opts = {}) {
 const geminiReq = require('@derhuerst/gemini/client')
-const {makeFetch} = await import('make-fetch')
+  const { makeRoutedFetch } = await import('make-fetch')
+  const {fetch, router} = makeRoutedFetch()
 // const { Readable } = require('stream')
 
 const DEFAULT_OPTS = {
@@ -25,15 +26,12 @@ const DEFAULT_OPTS = {
     return theData
   }
 
-  return makeFetch(async (request) => {
-
+  async function handleGemini(request) {
     const { url, method, headers: reqHeaders, body, signal, referrer } = request
 
     if(signal){
       signal.addEventListener('abort', takeCareOfIt)
     }
-
-    try {
       const toRequest = new URL(url, referrer)
 
       if (toRequest.protocol !== 'gemini:') {
@@ -71,8 +69,9 @@ const DEFAULT_OPTS = {
         })
       })
       return sendTheData(signal, mainObj)
-    } catch (error) {
-      return sendTheData(signal, {statusCode: 500, headers: {'Content-Type': mainRes}, data: mainReq ? [`<html><head><title>Gemini-Handle</title></head><body>${JSON.stringify(error.stack)}</body></html>`] : [JSON.stringify(error.stack)]})
-    }
-  })
+  }
+
+  router.any('gemini://*/**', handleGemini)
+
+  return fetch
 }
